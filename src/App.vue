50f2c2779472cfc33b2c7c95cfd7d943885b0f20
mslmyrtd <mslmyrtd@gmail.com>
2022-09-10 16:33:49 +0300

    appvue edited

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-<p class="text-3xl font-bold underline">Hello</p>
-  <RouterView />
+    <Navbar />
+  
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
