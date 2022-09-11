569dfc31ab8e226c8a356154f0cb52adf750b6c7
mslmyrtd <mslmyrtd@gmail.com>
2022-09-11 22:13:40 +0300

    cretae function for modal component and description for question

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,14 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
+
 <template>
   <header class="sticky top-0 bg-slate-200 shadow-lg">
     <nav class="container flex flex-col sm:flex-row items-center gap-4 py-6">
@@ -31,6 +39,7 @@ import { RouterLink } from "vue-router";
           stroke-width="1.5"
           stroke="currentColor"
           class="w-6 h-6 text-xl hover:text-slate-400 duration-150 cursor-pointer"
+          @click="toggleModal"
         >
           <path
             stroke-linecap="round"
@@ -53,6 +62,36 @@ import { RouterLink } from "vue-router";
           />
         </svg>
       </div>
+      <Modal :modalActive="modalActive" @close-modal='toggleModal'>
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Weather App allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </Modal>
     </nav>
   </header>
 </template>
