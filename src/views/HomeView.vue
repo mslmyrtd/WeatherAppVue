cef3312c95e760ce418da7953159689f644f6407
mslmyrtd <mslmyrtd@gmail.com>
2022-09-15 22:33:07 +0300

    added message for no matching search and not find anything

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,19 +6,23 @@ const mapboxAPIKey =
 
 const mapboxSearchResults = ref(null);
 const searchQuery = ref("");
-
+const searchError=ref(null)
 const queryTimeout = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
+      try {
         const result = await axios.get(
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
         );
         mapboxSearchResults.value = result.data.features;
-      mapboxSearchResults.value=result.data.features;
-      console.log(result.data.features);
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value=true
+      }
+
       return;
     }
     mapboxSearchResults.value = null;
@@ -34,10 +38,23 @@ const getSearchResults = () => {
         placeholder="Search for a city or state"
         v-model="searchQuery"
         @input="getSearchResults"
-        class="py-2 px-1 w-full bg-transparent border-b focus:border-slate-300 focus:outline-none focus:shadow-sm"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-white focus:outline-none focus:shadow-sm"
       />
-      <ul class="absolute bg-slate-300 w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer hover:bg-slate-400">{{searchResult.place_name}}</li>
+      <ul
+        class="absolute bg-slate-300 w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+      <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+      <p v-if="mapboxSearchResults.length===0">No results match your query, try a different term.</p>
+      <template v-else>
+          <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer pl-1 hover:bg-slate-400"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </template>
       </ul>
     </div>
   </main>
