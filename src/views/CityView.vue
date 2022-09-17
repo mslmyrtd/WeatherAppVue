13dfa876b23557877fe4277e7160b81dda8cc2da
mslmyrtd <mslmyrtd@gmail.com>
2022-09-17 16:08:22 +0300

    asyncCityViec called with suspend

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <Suspense>
+      <AsyncCityView />
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script setup>
-
+import AsyncCityView from "@/components/AsyncCityView.vue";
 </script>
