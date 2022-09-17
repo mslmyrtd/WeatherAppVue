d4971be66ba89af273f4df6e4e3aea2e5ba9282d
mslmyrtd <mslmyrtd@gmail.com>
2022-09-17 23:13:23 +0300

    added banner and weatheroverview

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -23,7 +23,7 @@ const getWeatherData = async () => {
       const utc = hour.dt * 1000 + localOffset;
       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
     });
-    return weatherData;
+    return weatherData.data;
   } catch (error) {
     console.log(error);
   }
@@ -33,5 +33,41 @@ console.log(weatherData);
 </script>
 
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center" >
+    <!-- Banner -->
+    <div class=" p-4 bg-slate-300 w-full text-center" v-if="route.query.preview" >
+    <p>You are currently previewing this city, click the "+" icon to start tracking this city.</p>
+    </div>
+    <!-- Weather Overview -->
+<div class="flex flex-col items-center py-12" >
+  <h1 class="text-4xl mb-2">{{route.params.city}}</h1>
+  <p class="text-sm mb-12">
+    {{
+      new Date(weatherData.currentTime).toLocaleDateString(
+        "en-us",
+        {
+          weekday:"short",
+          day:"2-digit",
+          month:"long",
+        }
+      )
+    }}
+    {{
+      new Date(weatherData.currentTime).toLocaleTimeString(
+        "en-us",
+        {
+          timeStyle:"short",
+        }
+      )
+    }}
+  </p>
+  <p class="text-8xl mb-8">{{Math.round(weatherData.current.temp)}}&deg;</p>
+  
+    <p>Feels like {{Math.round(weatherData.current.feels_like)}}&deg;</p>
+    <p class="capitalize">{{weatherData.current.weather[0].description}}&deg;</p>
+    <img class="w-[150px] h-auto" :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt="">
+
+</div>
+<hr class="border-black border-opacity-10 border w-full" />
+  </div>
 </template>
