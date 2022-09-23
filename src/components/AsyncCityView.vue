962695a1358b1901a91a8d3717ebe4c9b5978245
mslmyrtd <mslmyrtd@gmail.com>
2022-09-23 23:04:25 +0300

    added 7 day forecast

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -87,7 +87,29 @@ console.log(weatherData);
     </div>
   </div>
 </div>
-
-
+<hr class="border-black border-opacity-10 border w-full">
+<!--Weekly Weather -->
+<div class="max-w-screen-md w-full py-12">
+  <div>
+    <h2 class="mb-4">7 Day Forecast</h2>
+    <div v-for="day in weatherData.daily" :key="day.dt" class="flex items-center">
+    <p class="flex-1">
+      {{
+        new Date(day.dt*1000).toLocaleDateString(
+          "en-us",
+          {
+            weekday:"long"
+          }
+        )
+      }}
+    </p>
+    <img :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="" class="w-[50px] h-[50px] object-cover">
+<div class="flex gap-2 flex-1 justify-end">
+<p>H: {{Math.round(day.temp.max)}}</p>
+<p>L: {{Math.round(day.temp.min)}}</p>
+</div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
