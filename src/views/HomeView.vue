e665f36614d88088b8363fc94d822b8497bc4b12
mslmyrtd <mslmyrtd@gmail.com>
2022-10-30 00:28:20 +0300

    added CityList

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
+import CityList from "@/components/CityList.vue";
 
 const router=useRouter()
 const mapboxAPIKey =
@@ -73,6 +74,14 @@ const getSearchResults = () => {
         </li>
       </template>
       </ul>
+    </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList/>
+        <template #fallback>
+          <p>Loading...</p>
+        </template>
+      </Suspense>
     </div>
   </main>
 </template>
