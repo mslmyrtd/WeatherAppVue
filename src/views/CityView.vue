d5146760b0de6f33349e79764b6d3f6ead6bc43d
mslmyrtd <mslmyrtd@gmail.com>
2022-11-14 09:21:02 +0300

    edited

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,14 +1,16 @@
+<script setup>
+import AsyncCityView from "@/components/AsyncCityView.vue";
+import CityViewSkeleton from "@/components/CityViewSkeleton.vue";
+</script>
 <template>
   <div>
     <Suspense>
       <AsyncCityView />
       <template #fallback>
-        <p>Loading...</p>
+      <CityViewSkeleton/>
       </template>
     </Suspense>
   </div>
 </template>
 
-<script setup>
-import AsyncCityView from "@/components/AsyncCityView.vue";
-</script>
+
