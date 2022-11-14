b22e56c8fca22bf319d76d510c354c5a9c362d67
mslmyrtd <mslmyrtd@gmail.com>
2022-11-14 09:16:27 +0300

    cityCardSkeleton added

---
+++ src/components/CityCardSkeleton.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AnimatedPlaceholder from "@/components/AnimatedPlaceholder.vue"
+
+</script>
+
+
+<template>
+  <div class="flex py-6 px-3 bg-blue-300 rounded-md shadow-md">
+    <div class="flex flex-col flex-1 gap-2">
+      <AnimatedPlaceholder class="max-w-[50%]" />
+      <AnimatedPlaceholder class="max-w-[40%]" />
+    </div>
+    <div class="flex flex-col items-end flex-1 gap-2">
+         <AnimatedPlaceholder class="max-w-[50px] w-full" />
+      <AnimatedPlaceholder class="max-w-[75px] w-full" />
+    </div>
+  </div>
+</template>
